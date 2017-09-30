243246f883e72c4b0e7ff7d114b8f0010774174c
Marko Niemi <marko.j.niemi@gmail.com>
2017-09-30 13:52:06 +0300

    WebSecurity using UserServiceAuthrnticationProvider. Selenium tests.

---
+++ src/main/java/org/survey/security/UserRepositoryAuthenticationProvider.java
@@ -0,0 +1,62 @@
+package org.survey.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+import org.survey.model.user.User;
+import org.survey.service.user.UserService;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Spring security AuthenticationProvider which authenticates using
+ * UserRepository.
+ * 
+ * @see http://tedyoung.me/2011/06/21/spring-security-custom-authenticators/
+ * @see http 
+ *      ://samerabdelkafi.wordpress.com/2011/01/16/secure-your-web-application
+ *      -with-spring-security/
+ */
+@Slf4j
+@Component
+public class UserRepositoryAuthenticationProvider implements
+        AuthenticationProvider {
+    @Resource
+    UserService userService;
+
+    /**
+     * Authenticate using UserRepository.
+     */
+    @Override
+    public Authentication authenticate(Authentication authentication) {
+        log.debug("authenticate {}", authentication.getName());
+        User user = userService.findOne(authentication.getName());
+        return authenticateUser(user, authentication);
+    }
+
+    private Authentication authenticateUser(User user,
+            Authentication authentication) {
+        if (user != null
+                && authentication.getCredentials().equals(user.getPassword())) {
+            List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+            authorities.add(new SimpleGrantedAuthority(user.getRole().name()));
+            return new UsernamePasswordAuthenticationToken(
+                    authentication.getName(), authentication.getCredentials(),
+                    authorities);
+        } else {
+            return null;
+//            throw new BadCredentialsException("Authentication failed");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<? extends Object> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
