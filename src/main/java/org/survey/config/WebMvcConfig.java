dc88d9c6e69ecb250fe7c22443b7c7adc8c9441c
Marko Niemi <marko.j.niemi@gmail.com>
2021-05-04 11:46:35 +0300

    webjars-locator.

--- src/main/java/org/survey/config/WebMvcConfig.java
+++ src/main/java/org/survey/config/WebMvcConfig.java
@@ -14,9 +14,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/").resourceChain(false);
         registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
     }
+
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
